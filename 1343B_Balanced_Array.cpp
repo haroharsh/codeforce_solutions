444fad5b633f2e9ab26aca81b522b031f4a20f2d
Harshit Choudhary <harshitnariyal@gmail.com>
2026-08-23 21:06:52 +0530

    Sync Codeforces: 1343B - Balanced Array [C++23 (GCC 14-64, msys2)]

---
+++ 1343B_Balanced_Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n % 4 != 0) {
+            cout << "NO" <<endl;
+            continue;
+        }
+        cout << "YES" <<endl;
+        for (int i = 1; i <= n / 2; i++) {
+            cout << i * 2 << " ";
+        }
+        for (int i = 1; i < n / 2; i++) {
+            cout << (i * 2) - 1 << " ";
+        }
+        cout << (n / 2 * 2) - 1 + (n / 2) << endl;
+    }
+    return 0;
+}
