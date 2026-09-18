e31cdd724f4d17430ff3e0a4bef973113cb77b20
Harshit Choudhary <harshitnariyal@gmail.com>
2026-09-18 21:07:41 +0530

    Sync Codeforces: 151A - Soft Drinking [C++23 (GCC 14-64, msys2)]

---
+++ 151A_Soft_Drinking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+ 
+    int drink = (k * l) / nl;
+    int lime = c * d;
+    int salt = p / np;
+ 
+    int totalToasts = min({drink, lime, salt});
+ 
+    cout << totalToasts / n;
+ 
+    return 0;
+}
